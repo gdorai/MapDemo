59799114b561737acf7b26ab83f71bda72e97cfd
Jon Friskics <jonfriskics@gmail.com>
2013-04-23 22:21:52 -0400

    Initial Commit

---
+++ MapDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by Jon Friskics on 4/23/13.
+//  Copyright (c) 2013 Code School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
